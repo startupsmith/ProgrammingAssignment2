c051e5fe42d4a6c672234b57e7e7027813241b03
User Account <user@localhost.localdomain>
2014-10-26 13:37:17 -0400

    Added comments and usage examples in cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 
 
 ## Create a special "matrix" object that encapsulates an actual matrix and
-## can cache the inverse of that matrix.
+## can cache the inverse of that matrix. Example use:
+##      amatrix = makeCacheMatrix(matrix(c(1,2,3,4), nrow=2, ncol=2))
+##      amatrix$get()         # Returns original matrix
+##      [,1] [,2]
+## [1,]    1    3
+## [2,]    2    4
+##      amatrix$set(matrix(c(0,5,99,66), nrow=2, ncol=2)) # Modify existing matrix
+##  After calling cacheSolve(amatrix), one may then call
+##      amatrix$getinverse()  # Returns matrix inverse
+##      [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
 makeCacheMatrix <- function(x = matrix()) {
     cachedInverse <- NULL
@@ -25,6 +36,8 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Computes the inverse of the special "matrix" returned by makeCacheMatrix
 ## above. If the inverse has already been calculated (and the matrix has not
 ## changed), then the cachesolve will retrieve the inverse from the cache.
+## Example use:
+##      cacheSolve(amatrix)
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
